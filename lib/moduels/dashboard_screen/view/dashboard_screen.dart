4b57c951d2de0868d242d7c60611b6e8bce0bbe2
unknown <mohamed_elshereef@germaniatek.net>
2023-11-02 15:58:07 +0200

    create app screens

---
+++ lib/moduels/dashboard_screen/view/dashboard_screen.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
